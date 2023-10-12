a0809651e09e8b5561631ece36efeb7bf8b4692f
Rio Edwards <rioredwards@gmail.com>
2023-10-12 11:36:58 -0700

    Calling javascript from python and printing output

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import subprocess
 
-subprocess.run("cat hello.txt", shell=True)
+# Print out file contents
+# result = subprocess.run(["cat", "hello.txt"], capture_output=True, text=True)
+# print(result.stdout)
+
+# Execute Node.js script and print out result
+result = subprocess.run(["node", "example.js"], capture_output=True)
+print(result.stdout.decode("utf-8"))
