cd7c4e446fd9ddc7757e9b4ee6993a678ae01888
Rio Edwards <rioredwards@gmail.com>
2023-10-12 14:20:43 -0700

    Sending arguments in subprocess and writing response to file in python

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
+import { Entry, EntrySkeletonType } from "contentful";
 import { convertRichTextToHTML, getProject } from "./contentfulMarkdownRenderer.js";
 import { writeToFile } from "./writeToFile.js";
-import { NodeHtmlMarkdown, NodeHtmlMarkdownOptions } from "node-html-markdown";
+import { NodeHtmlMarkdown } from "node-html-markdown";
 
-console.log("Hello From Typescript!");
+const specifiedRichTextField = process.argv[2];
+console.log("specifiedRichTextField: ", specifiedRichTextField);
+if (!specifiedRichTextField || typeof specifiedRichTextField !== "string") {
+  console.error("Please provide a rich text field name as an argument.");
+  process.exit(1);
+}
 
 async function main() {
-  const project = await getProject("5kwgY0nLn1cdS4Z9GTZXos");
-  const rawRichTextField = project.fields.slogan;
-  // console.log("rawRichTextField: ", rawRichTextField);
+  // Fetch the project from Contentful
+  let project: Awaited<ReturnType<typeof getProject>>;
+  try {
+    project = await getProject("5kwgY0nLn1cdS4Z9GTZXos");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Convert the rich text field to HTML
+  const rawRichTextField = project.fields[specifiedRichTextField];
   const html = convertRichTextToHTML(rawRichTextField);
-  // console.log(html);
-  writeToFile(html, "index.html");
-  const markdown = NodeHtmlMarkdown.translate(
-    /* html */ html,
-    /* options (optional) */ {},
-    /* customTranslators (optional) */ undefined,
-    /* customCodeBlockTranslators (optional) */ undefined
-  );
-  writeToFile(markdown, "index.md");
+  const markdown = NodeHtmlMarkdown.translate(html);
+
+  // writeToFile(html, "index.html");
+  // writeToFile(markdown, "index.md");
+
   console.log(markdown);
 }
 
